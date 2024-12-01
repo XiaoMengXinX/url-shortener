b0ad28578b1779fd39b44d2dbcf1e6e6fc62da93
xiaomengxin <xibaole2333@gmail.com>
2024-12-01 18:57:20 +0800

    fix

--- api/url.go
+++ api/url.go
@@ -2,10 +2,55 @@ package api
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 	"net/http"
+	"os"
+	"regexp"
+	"time"
 )
+
+var collection *mongo.Collection
+var re = regexp.MustCompile("(http|https)://[\\w\\-_]+(\\.[\\w\\-_]+)+([\\w\\-.,@?^=%&:/~+#]*[\\w\\-@?^=%&/~+#])?")
+
+type urlData struct {
+	Token     string    `bson:"token"`
+	URL       string    `bson:"url"`
+	CreatedAt time.Time `bson:"created_at"`
+}
+
+type resData struct {
+	Token string `json:"token"`
+	Error string `json:"error"`
+}
+
+func connectDB(uri string) (*mongo.Client, error) {
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	clientOptions := options.Client().
+		ApplyURI(uri).
+		SetServerAPIOptions(serverAPIOptions)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	return mongo.Connect(ctx, clientOptions)
+}
+
+func responseJson(r resData) string {
+	b, _ := json.Marshal(r)
+	return string(b)
+}
+
+func init() {
+	client, err := connectDB(os.Getenv("MONGO_URI"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("short_url").Collection("urls")
+}
 
 func RedirectHandler(w http.ResponseWriter, r *http.Request) {
 	if len(r.URL.Path) <= 1 {
